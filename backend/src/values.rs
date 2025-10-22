9aae883a481a09961f1708699ad41eefd8bc5ddc
Lef Ioannidis <elefthei@seas.upenn.edu>
2025-10-21 22:53:12 -0400

    Fix ifft and fft test

--- backend/src/values.rs
+++ backend/src/values.rs
@@ -424,7 +424,7 @@ impl<C: ArkConfig> Value<C> {
                     *other = Value::Scalar(value);
                 },
                 // Scalar * Scalar = Scalar
-                Value::Scalar(_) => { 
+                Value::Scalar(_) => {
                     C::FOps::mul(a, other.into_scalar_mut())
                 },
                 // Index * groups
@@ -958,7 +958,7 @@ impl<C: ArkConfig> Value<C> {
                             C::G1Ops::mul(&f_inv, &mut gm);
                             gm
                         })
-                        .collect(), 
+                        .collect(),
                     );
                 }
                 // Vec<Group1> / Vec<Index>
@@ -993,7 +993,7 @@ impl<C: ArkConfig> Value<C> {
                             C::G2Ops::mul(&f_inv, &mut gm);
                             gm
                         })
-                        .collect(), 
+                        .collect(),
                     );
                 }
                 // Vec<Group1> / Vec<Index>
@@ -1196,7 +1196,7 @@ impl<C: ArkConfig> Value<C> {
             (Value::VecIndex(_), Value::VecG1(b)) => {
                 let vg = b.par_iter().map(|a| (*a).into()).collect::<Vec<_>>();
                 *other = Value::VecG1Affine(vg);
-                Self::value_dot(self, other);  
+                Self::value_dot(self, other);
             }
             (Value::VecG2(b), Value::VecIndex(_)) => {
                 let vg = b.par_iter().map(|a| (*a).into()).collect::<Vec<_>>();
@@ -1209,7 +1209,7 @@ impl<C: ArkConfig> Value<C> {
             (Value::VecScalar(_), Value::VecG1(b)) => {
                 let vg = b.par_iter().map(|a| (*a).into()).collect::<Vec<_>>();
                 *other = Value::VecG1Affine(vg);
-                Self::value_dot(self, other); 
+                Self::value_dot(self, other);
             }
             (Value::VecG2(b), Value::VecScalar(_)) => {
                 let vg = b.par_iter().map(|a| (*a).into()).collect::<Vec<_>>();
@@ -1218,8 +1218,8 @@ impl<C: ArkConfig> Value<C> {
             (Value::VecScalar(_), Value::VecG2(b)) => {
                 let vg: Vec<<C as ArkConfig>::G2Affine> = b.par_iter().map(|a| (*a).into()).collect::<Vec<_>>();
                 *other = Value::VecG2Affine(vg);
-                Self::value_dot(self, other); 
-            } 
+                Self::value_dot(self, other);
+            }
             (Value::VecIndex(a), Value::VecIndex(b)) => {
                 *other = Value::Index(a.par_iter().zip(b.par_iter()).map(|(a, b)| *a * *b).sum())
             }
@@ -2019,7 +2019,7 @@ impl<C: ArkConfig> Value<C> {
                     _ => panic!("Expected Vec or VecIndex, found {}", vec_value)
                 }
             },
-            ATyp::Base(ABase::G1) => { vec_value.into_vec_g1_mut(); vec_value }, 
+            ATyp::Base(ABase::G1) => { vec_value.into_vec_g1_mut(); vec_value },
             ATyp::Base(ABase::G2) => { vec_value.into_vec_g2_mut(); vec_value },
             ATyp::Base(ABase::GT) => { vec_value.into_vec_gt_mut(); vec_value },
             _ => panic!("Not yet implemented for vector")
@@ -2547,7 +2547,7 @@ fn test_mul_comm() {
     let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::scalar());
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::scalar());
     assert_deq!(&a * &b, &b * &a);
-    
+
     // Vec<Scalar> * Vec<Scalar>
     let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::Vec(Box::new(ATyp::scalar()), 10));
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::Vec(Box::new(ATyp::scalar()), 10));
@@ -2575,12 +2575,12 @@ fn test_mul_comm() {
 #[test]
 fn coef_eval_test() {
     let mut rng = test_rng();
-    let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(15));
-    let c = ((&a).value_fft()).value_ifft();
-    // assert_deq!(&c, &a);
-    
-    let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(15));
-    let c = ((&a).value_ifft()).value_fft();
+    let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(8));
+    let c = a.value_fft().value_ifft();
+    assert_deq!(&c, &a);
+
+    let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(8));
+    let c = a.value_ifft().value_fft();
     assert_deq!(&c, &a);
 }
 
@@ -2609,7 +2609,7 @@ fn inverse_test() {
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::scalar());
     let c = (&a * &b) / b;
     assert_deq!(&a, &c);
-    
+
     let a = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_g2(10));
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::scalar());
     let c = (&a * &b) / b;
@@ -2636,12 +2636,12 @@ fn inverse_test() {
     let a = Value::<ArkBls12_381>::random(&mut rng, &&ATyp::vec_g1(10));
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(10));
     let c = &(&b.clone() / &b.clone()) * &a;
-    assert_deq!(&a, &c); 
+    assert_deq!(&a, &c);
 
     let a = Value::<ArkBls12_381>::random(&mut rng, &&ATyp::vec_g2(10));
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(10));
     let c = &(&b.clone() / &b.clone()) * &a;
-    assert_deq!(&a, &c); 
+    assert_deq!(&a, &c);
 
     let a = Value::<ArkBls12_381>::scalar_from_usize(1);
     let b = Value::<ArkBls12_381>::random(&mut rng, &ATyp::vec_scalar(10));
