a70c3adfe908ad4f021a9110b2ad51f710edefe0
Sebastian Angel <sebastian.angel@cis.upenn.edu>
2026-03-10 15:31:13 -0400

    cleaning up the code

--- examples/dory/main.rs
+++ examples/dory/main.rs
@@ -1,9 +1,8 @@
 use zippel::*;
 use std::{path::PathBuf, time::Instant};
-use backend::{ArkBls12_381, ArkConfig, Value, ArkGroupOps, ArkPairingOps, ArkScalarOps};
+use backend::{ArkBls12_381, ArkConfig, Value, ArkGroupOps, ArkPairingOps};
 use lang::id::Vid;
 use share::Ctx;
-use ark_std::UniformRand;
 
 fn main() {
     println!("=== Dory Evaluation Proof (ArkBls12_381) ===");
@@ -12,6 +11,7 @@ fn main() {
     println!("Compiling Zippel files...");
     handler.compile();
     println!("Compilation successful.");
+    
 
     let inputs = prover_create_inputs();
     println!("Generating default schedule for prover...");
@@ -64,11 +64,11 @@ fn prover_create_inputs() -> Ctx<Vid, Value<ArkBls12_381>> {
     let gamma_pair_ipp = <ArkBls12_381 as ArkConfig>::POps::billinear_vec_dot(&gamma1, &gamma2);
     
     // Hash L and R are just pairings of gamma halves
-    let gamma1_L = gamma1[0..n/2].to_vec();
-    let gamma1_R = gamma1[n/2..n].to_vec();
+    let gamma1_l = gamma1[0..n/2].to_vec();
+    let gamma1_r = gamma1[n/2..n].to_vec();
     
-    let hash_L = <ArkBls12_381 as ArkConfig>::POps::billinear_vec_dot(&gamma1_L, &gamma2_prime);
-    let hash_R = <ArkBls12_381 as ArkConfig>::POps::billinear_vec_dot(&gamma1_R, &gamma2_prime);
+    let hash_l = <ArkBls12_381 as ArkConfig>::POps::billinear_vec_dot(&gamma1_l, &gamma2_prime);
+    let hash_r = <ArkBls12_381 as ArkConfig>::POps::billinear_vec_dot(&gamma1_r, &gamma2_prime);
 
     Ctx::<Vid, Value<ArkBls12_381>>::from_iter([
         (Vid("c1".to_string()), Value::GT(c1)),
@@ -78,8 +78,8 @@ fn prover_create_inputs() -> Ctx<Vid, Value<ArkBls12_381>> {
         (Vid("gamma2".to_string()), Value::VecG2(gamma2)),
         (Vid("gamma1_prime".to_string()), Value::VecG1(gamma1_prime)),
         (Vid("gamma2_prime".to_string()), Value::VecG2(gamma2_prime)),
-        (Vid("hash_L".to_string()), Value::GT(hash_L)),
-        (Vid("hash_R".to_string()), Value::GT(hash_R)),
+        (Vid("hash_l".to_string()), Value::GT(hash_l)),
+        (Vid("hash_r".to_string()), Value::GT(hash_r)),
         (Vid("gamma_pair_ipp".to_string()), Value::GT(gamma_pair_ipp)),
         (Vid("u_vec".to_string()), Value::VecG1(u_vec)),
         (Vid("g_vec".to_string()), Value::VecG2(g_vec)),
